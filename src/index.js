e218b2d5cff54e008e2ecc91121a51d5ddd45342
xClune <aclune382@gmail.com>
2024-04-11 09:19:28 +0800

    Add tab functionality between home about and menu. Add css styling for header.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-
+import "./styles.css"
 
 console.log('test message')
+
+// Event Listeners 
+
+const home = document.querySelector('.home-btn')
+const menu = document.querySelector('.menu-btn')
+const about = document.querySelector('.about-btn')
+
+const content = document.getElementById('content');
+
+home.addEventListener('click', () => {
+    content.innerText = homeContent;
+})
+
+menu.addEventListener('click', () => {
+    content.innerText = menuContent;
+})
+
+about.addEventListener('click', () => {
+    content.innerText = aboutContent;
+})
+
+const homeContent = "Content for home page";
+const menuContent = "Content for menu page";
+const aboutContent = "Content for about page";
